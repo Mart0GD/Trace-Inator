b167cce7552b204b31e94522530e1514851e742c
Mart0GD <marty0545@gmail.com>
2026-08-13 11:07:42 +0300

    feat: added colors

---
+++ include/math/color.hpp
@@ -0,0 +1,25 @@
+#ifndef __COLOR_HPP_INCLUDED__
+#define __COLOR_HPP_INCLUDED__
+
+#include "math/vec3.hpp"
+#include <iostream>
+
+typedef vec3 color;
+
+inline void print_color(std::ostream& os, const color& pixel)
+{
+    color final_color;
+
+    // Reinhard tone mapping --> saw it on youtube 
+    final_color.x = pixel.x / (pixel.x + 1.0);
+    final_color.y = pixel.y / (pixel.y + 1.0);
+    final_color.z = pixel.z / (pixel.z + 1.0);
+
+    int red_scaled   = (int)(final_color.x * 255.999);  // x --> red
+    int green_scaled = (int)(final_color.y * 255.999);  // y --> green
+    int blue_scaled  = (int)(final_color.z * 255.999);  // z --> blue
+
+    os << red_scaled << ' ' << green_scaled << ' ' << blue_scaled << '\n';
+}
+
+#endif
