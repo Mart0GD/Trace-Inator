264c2ac4044eee197ff4d8d8223c5bd06c2a1fcc
Mart0GD <marty0545@gmail.com>
2026-08-13 13:12:16 +0300

    feat: added timer to entry point

--- apps/main.cpp
+++ apps/main.cpp
@@ -3,6 +3,8 @@
 #include "camera.hpp"
 #include "renderer.hpp"
 
+#include <chrono>
+#include <iostream>
 
 int main()
 {   
@@ -11,6 +13,15 @@ int main()
     renderer r;
     r.world = &world;
 
+    std::chrono::high_resolution_clock::time_point start = std::chrono::high_resolution_clock::now();
+    
     r.render();
+    
+    std::chrono::high_resolution_clock::time_point end = std::chrono::high_resolution_clock::now();
+
+    std::chrono::microseconds duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+
+    const double seconds = duration.count() / 1'000'000.0;
+    std::cout << "time for exectution: " << seconds << " seconds." << std::endl;
     return 0;
 }
