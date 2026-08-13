8d91902f78582cb069b9faf9856236dd0fb42bd7
Mart0GD <marty0545@gmail.com>
2026-08-13 11:23:51 +0300

    feat: added base logic for hitable geometry

---
+++ include/geometry/hitable.hpp
@@ -0,0 +1,25 @@
+#ifndef __HITABLE_HPP_INCLUDED__
+#define __HITABLE_HPP_INCLUDED__
+
+#include "utils/constants.hpp"
+
+struct hit_record
+{
+    point3D point;                              // <- 3D point of where the ray hits the geometry
+    vec3    hit_normal;                         // <- interpolated vertex normal
+    vec3    geometric_normal;                   // <- the face normal of the geometry
+    
+    double  t;                                  // <- distance in time from the origin
+    size_t  material_id;                        // <- material in the scene (no copies in mesh)
+    double  baryU,baryV;                        // <- barycentric coordinates
+    point3D pUV;                                // <- interpolated u,v coordinates
+};
+
+class hitable{
+public:
+    virtual ~hitable() noexcept = default;
+
+    virtual bool hit(const ray& r, double t_min, double t_max, hit_record& rec) const = 0;
+};
+
+#endif
