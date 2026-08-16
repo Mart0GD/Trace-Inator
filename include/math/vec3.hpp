7d85c0e000c8e7d12663da657f969697c5c1dc77
Mart0GD <marty0545@gmail.com>
2026-08-16 12:02:55 +0300

    feat: added indexing of axis

--- include/math/vec3.hpp
+++ include/math/vec3.hpp
@@ -1,17 +1,28 @@
 #ifndef __VEC3_HPP_INCLUDED__
 #define __VEC3_HPP_INCLUDED__
 
+#include "utils/defines.hpp"
+
 #include <cmath>
 #include <iostream>
+#include <cstdint>
 
 struct vec3 {
-    double x,y,z;
+    fp x,y,z;
 
     vec3() : x(0), y(0), z(0) {};
-    vec3(double x, double y, double z) : x(x), y(y), z(z) {};
+    vec3(fp x, fp y, fp z) : x(x), y(y), z(z) {};
 
-    double length()         const { return std::sqrt(x * x + y * y + z * z); }
-    double length_pow2()    const { return x * x + y * y + z * z; }
+    fp length()         const { return std::sqrt(x * x + y * y + z * z); }
+    fp length_pow2()    const { return x * x + y * y + z * z; }
+
+    fp    operator [] (const uint32_t index)
+    {
+        ASSERT_OR_THROW(index < 3);
+        if(index == 0) return x;
+        if(index == 1) return y;
+        return z;
+    }
 
     vec3& operator += (const vec3& other) 
     {
@@ -39,7 +50,7 @@ struct vec3 {
     friend std::ostream& operator << (std::ostream& os, const vec3& vec);
 };
 
-typedef vec3 point3D;
+// -- OPERATIONS --
 
 inline vec3 operator - (const vec3& vec) { return vec3(-vec.x, -vec.y, -vec.z); }
 
@@ -75,7 +86,7 @@ inline vec3 operator / (const vec3& vec, double scalar)
 }
 
 
-inline double dot(const vec3& vec1, const vec3& vec2) {
+inline fp dot(const vec3& vec1, const vec3& vec2) {
     return vec1.x * vec2.x
         +  vec1.y * vec2.y
         +  vec1.z * vec2.z;
@@ -91,22 +102,10 @@ inline vec3 unit_vector(const vec3& v) {
     return v / v.length();
 }
 
-inline double dist(const point3D& p1, const point3D& p2)
-{
-    return sqrt
-    (
-        (p2.x - p1.x) * (p2.x - p1.x) + 
-        (p2.y - p1.y) * (p2.y - p1.y) +
-        (p2.z - p1.z) * (p2.z - p1.z) 
-    );
-}
-
-
 inline std::ostream& operator << (std::ostream& os, const vec3& vec)
 {
     os << "(" << vec.x << ", " << vec.y << ", " << vec.z << ")";
     return os;
 }
-
 
 #endif
