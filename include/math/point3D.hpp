1dbaa610fbc59ec3b0d61fab07e021bd7cb12827
Mart0GD <marty0545@gmail.com>
2026-08-16 19:16:35 +0300

    feat: separated point logic from vector

---
+++ include/math/point3D.hpp
@@ -0,0 +1,29 @@
+#ifndef __POINT3D_HPP_INCLUDED__
+#define __POINT3D_HPP_INCLUDED__
+
+#include "math/vec3.hpp"
+
+typedef vec3 point3D;
+
+inline fp dist(const point3D& p1, const point3D& p2)
+{
+    return sqrt
+    (
+        (p2.x - p1.x) * (p2.x - p1.x) + 
+        (p2.y - p1.y) * (p2.y - p1.y) +
+        (p2.z - p1.z) * (p2.z - p1.z) 
+    );
+}
+
+inline point3D min(const point3D& p1, const point3D& p2)
+{
+    return point3D(std::min(p1.x,p2.x), std::min(p1.y,p2.y), std::min(p1.z,p2.z));
+}
+
+inline point3D max(const point3D& p1, const point3D& p2)
+{
+    return point3D(std::max(p1.x,p2.x), std::max(p1.y,p2.y), std::max(p1.z,p2.z));
+}
+
+
+#endif
