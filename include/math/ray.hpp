09d38d95f38beddf70897586216a9c2668d8571e
Mart0GD <marty0545@gmail.com>
2026-08-21 00:00:19 +0300

    feat: updated comments

--- include/math/ray.hpp
+++ include/math/ray.hpp
@@ -3,6 +3,7 @@
 
 #include "math/vec3.hpp"
 #include "math/point3D.hpp"
+#include "math/color.hpp"
 
 enum r_type
 {
@@ -12,18 +13,19 @@ enum r_type
     RT_SHADOW,
     RT_REFLECTION,
     RT_REFRACTION,
+    RT_DIFFUSE,
 
     RT_COUNT
 };
 
 struct ray {
 
-    point3D origin      = point3D(0,0,0);   // <- origin point in 3D world
-    vec3    direction   = vec3(0,0,-1);     // <- direction vector
-    int32_t depth       = 0;                // <- how many rays came before 
-    r_type  type        = RT_CAMERA;        // <- type of the ray for logic checks
-    fp  ior             = 1;                // <- ior of the material where the ray comes from
-    vec3    inv_dir     = 1 / direction;    // <- for ray AABB intersections
+    point3D origin       = point3D(0,0,0);   // <- origin point in 3D world
+    vec3    direction    = vec3(0,0,-1);     // <- direction vector
+    int32_t depth        = 0;                // <- how many rays came before 
+    r_type  type         = RT_CAMERA;        // <- type of the ray for logic checks
+    fp  ior              = 1;                // <- ior of the material where the ray comes from
+    vec3    inv_dir      = 1 / direction;    // <- for ray AABB intersections
 
     inline point3D at(double length) const   { return origin + direction * length; }   
 };
